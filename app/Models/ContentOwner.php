9e1220072efd3467ec5eda427548bf893468c3b5
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-06-07 12:27:56 +0600

    working on login

---
+++ app/Models/ContentOwner.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContentOwner extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// admin routes
+Route::group(['prefix' => 'admin', 'name' => 'admin.'], function () {
+    Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
+});
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
