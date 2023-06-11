5bf4eb512124f42d19757013c47b2059ae46c679
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-06-11 14:47:55 +0600

    content create sucess

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -8,6 +8,8 @@ use App\Models\Content;
 use App\Models\ContentType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Finder\SplFileInfo;
 
 class ContentController extends Controller
 {
@@ -61,7 +63,7 @@ class ContentController extends Controller
             $image = $request->file('banner_image');
             $imageName = date('Y_m_d_H_i_s_') .  $image->getClientOriginalName();
             $image->move('upload/content/banner_image', $imageName);
-            $content->image = 'upload/content/banner_image/' . $imageName;
+            $content->banner_image = 'upload/content/banner_image/' . $imageName;
         }
         $content->description = $request->description;
         $content->artist_name = $request->artist_name;
@@ -71,14 +73,15 @@ class ContentController extends Controller
             $imageName = date('Y_m_d_H_i_s_') .  $image->getClientOriginalName();
             $image->move('upload/content/file', $imageName);
             $content->file_name = 'upload/content/file/' . $imageName;
+            $info = new SplFileInfo($content->file_name, '', '');
+            $content->file_size = $info->getSize();
         }
-        $content->file_size = "10";
         $content->location = $request->location;
         $content->insert_date = now();
         $content->mapping_status = 'Active';
         $content->owner_status = 'Active';
         $content->status = $request->status;
-        $content->created_by = "admin";
+        $content->created_by = auth()->user()->name;
 
         if ($content->save()) {
             $this->flashMessageSuccess('success', 'Content created successfully.');
